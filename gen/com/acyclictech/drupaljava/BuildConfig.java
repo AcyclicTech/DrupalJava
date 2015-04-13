4469a70986b7d42aee06553d8ea420c16866c3ea
jon <jon@ziggy>
2015-04-13 06:16:18 -0600

    json test messages

---
+++ gen/com/acyclictech/drupaljava/BuildConfig.java
@@ -0,0 +1,6 @@
+/** Automatically generated file. DO NOT MODIFY */
+package com.acyclictech.drupaljava;
+
+public final class BuildConfig {
+    public final static boolean DEBUG = true;
+}
--- src/com/acyclictech/drupaljava/services/json/JsonServiceBuilder.java
+++ src/com/acyclictech/drupaljava/services/json/JsonServiceBuilder.java
@@ -1,9 +1,26 @@
 package com.acyclictech.drupaljava.services.json;
 
+import org.json.JSONObject;
+
 public class JsonServiceBuilder {
 
+	
 	public void doLogin(){
-		
+		JsonObjectRequest jsObjRequest = new JsonObjectRequest
+		        (Request.Method.GET, url, null, new Response.Listener<JSONObject>() {
+
+		    @Override
+		    public void onResponse(JSONObject response) {
+		        mTxtDisplay.setText("Response: " + response.toString());
+		    }
+		}, new Response.ErrorListener() {
+
+		    @Override
+		    public void onErrorResponse(VolleyError error) {
+		        // TODO Auto-generated method stub
+
+		    }
+		});	
 	}
 	
 	public void doLogout(){
